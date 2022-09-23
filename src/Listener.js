28f80e1e7bd94a9cad3d386108174d8d3abc0dc5
farhan <farhanafriza24@gmail.com>
2022-09-23 23:00:50 +0700

    add comma

--- src/Listener.js
+++ src/Listener.js
@@ -10,7 +10,7 @@ class Listener {
       const { targetEmail, playlistId } = JSON.parse(message.content.toString());
       const playlistSongs = await this._playlistsService.getPlaylistSongsByPlaylistId(playlistId);
       const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify({
-        playlist: playlistSongs
+        playlist: playlistSongs,
       }));
       console.info(result);
     } catch (e) {
